583d1983b294b52bc52a746848896f95acf463a2
Monika <91907563+mkford4@users.noreply.github.com>
2022-06-26 13:34:38 -0500

    Added Gifted Chat, messages state, renderBubble & system message

--- components/Chat.js
+++ components/Chat.js
@@ -1,28 +1,82 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
   constructor() {
     super();
+    this.state = {
+      messages: [],
+    }
   };
 
-
-  render() {
+  componentDidMount() {
+    //renders user's name from Start page to top of chat
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
 
-    const { background } = this.props.route.params;
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: "You've entered the chat!",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    })
+  };
+
+  /* called when a user sends a message, appends new message to the messages object */
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#474056'
+          }
+        }}
+      />
+    )
+  }
+
+
+
+  render() {
+    let { chatBackground } = this.props.route.params;
 
     return (
-      <View style={styles.chatView}>
-        <Text>Hello World</Text>
+      <View style={{ backgroundColor: chatBackground, flex: 1 }}>
+        <GiftedChat
+
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null
+        }
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  chatView: {
-    flex: 1,
-  }
-})
